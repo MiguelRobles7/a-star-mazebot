2c9309f4608573ce3be427d30d536ec6597ce79f
qwerttyuiiop1 <64955571+qwerttyuiiop1@users.noreply.github.com>
2023-02-22 22:21:32 +0800

    optimal path and order of visit
    added optimal path and order of visit
    
    refactored some code

--- a_star_search.py
+++ a_star_search.py
@@ -7,27 +7,32 @@ class TileType(Enum):
 	END = 3
 # to keep track of the blocks of maze
 class Point:
-	def __init__(self, x, y):
+	def __init__(self, x: int, y: int):
 		self.x = x
 		self.y = y
 	def __lt__(self, _): #to fix issues on pq
 		return False
 
-def manhattan_distance(curr_node,dest):
-	return (abs(curr_node.x-dest.x)+abs(curr_node.y-dest.y))
+def manhattan_distance(src: Point, dst: Point):
+	return (abs(src.x-dst.x)+abs(src.y-dst.y))
 
-def getValidMoves(maze, point):
+def getValidMoves(maze: list[list[TileType]], point: tuple[int,int]) -> list[tuple[int,int]]:
 	size_x, size_y = len(maze), len(maze[0])
 	validMoves = []
 	for offset in [(1,0),(0,1),(-1,0),(0,-1)]:
-		x, y = point.x + offset[0], point.y + offset[1]
+		x, y = point[0] + offset[0], point[1] + offset[1]
 		if x < 0 or y < 0 or x >= size_x or y >= size_y:
 			continue
 		if maze[x][y] != TileType.WALL:
-			validMoves.append(Point(x, y))
+			validMoves.append((x, y))
 	return validMoves
 
-def A_Star(maze, end, start):
+def A_Star(
+	maze: list[list[TileType]], 
+	end: Point, start: Point) -> tuple[
+		list[tuple[int,int]],
+		list[tuple[int,int]]
+	]:
 
 	size_x, size_y = len(maze), len(maze[0]) #size of maze
 	pq = queue.PriorityQueue() # holds (priority, point)
@@ -42,17 +47,32 @@ def A_Star(maze, end, start):
 		curr = pq.get()[1]
 		if curr.x == end.x and curr.y == end.y:
 			break
-		parent_cost = costs[curr.x][curr.y]
-		for child in getValidMoves(maze, curr):
-			x, y = child.x, child.y
+		child_cost = costs[curr.x][curr.y] + 1 #the cost of each action is 1
+		for (x, y) in getValidMoves(maze, (curr.x, curr.y)):
 			if costs[x][y] != -1:
 				continue
 
-			costs[x][y] = parent_cost + 1 #the cost of each action is 1
+			costs[x][y] = child_cost
 			order.append((x, y))
 
-			pq.put((costs[x][y] + manhattan_distance(child, end), child))
-	return costs, order
+			child = Point(x, y)
+			pq.put((
+				costs[x][y] + manhattan_distance(child, end), 
+				child
+			))
+
+	end = (end.x, end.y)
+	start = (start.x, start.y)
+	optimal_path = [end]
+	while start != optimal_path[-1]:
+		a = optimal_path[-1]
+		child_cost = costs[a[0]][a[1]] - 1
+		for (x, y) in getValidMoves(maze, a):
+			if costs[x][y] == child_cost:
+				optimal_path.append((x,y))
+				break
+	optimal_path.reverse()
+	return optimal_path, order
 
 
 def printMaze(maze):
@@ -95,29 +115,35 @@ def main():
 	print(f"Start: {start.x}, {start.y} Destination: {goal.x}, {goal.y}")
 	
 	# Returns the closed list, we can use this to simulate making a maze
-	costs, path = A_Star(maze, goal, start)
-	count = 1 # to get explored order
-
-	for (x, y) in path:
-		maze[x][y] = f"{count:02}"
-		count+=1
+	optimal_path, order = A_Star(maze, goal, start)
 
 	for a in range(len(maze)):
 		for i in range(len(maze[0])):
 			if maze[a][i] == TileType.WALL:
 				maze[a][i] = "##"
-			elif maze[a][i] == TileType.EMPTY:
+			else:
 				maze[a][i] = ".."
+
+
+	count = 1 # to get explored order
+	for (x, y) in optimal_path:
+		maze[x][y] = f"{count:02}"
+		count+=1
 
 	print("Solved Maze Path: ")
 	for i in maze:
 		for a in i:
 			print(a, end=" ")
 		print()
-	print("\nCosts:")
-	for i in costs:
+
+	count = 1 # to get explored order
+	for (x, y) in order:
+		maze[x][y] = f"{count:02}"
+		count+=1
+	print("\nOrder:")
+	for i in maze:
 		for a in i:
-			print(f"{a:02}", end=" ")
+			print(a, end=" ")
 		print()
 
 if __name__ == '__main__':
